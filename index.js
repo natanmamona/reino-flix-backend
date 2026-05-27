8e81c0d1366c193c2cc78d4c02365c5039fa3a34
natanmamona <natanikki2010@gmail.com>
2026-05-27 00:34:08 -0300

    firebase render env

--- index.js
+++ index.js
@@ -8,11 +8,9 @@ const {
   Payment,
 } = require('mercadopago');
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 const db = admin.firestore();
 
