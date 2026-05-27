3dc649de3727cae9fa85a750f24f6269e2b91bac
natanmamona <natanikki2010@gmail.com>
2026-05-27 00:17:02 -0300

    usar firebase env

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const {
   Payment,
 } = require('mercadopago');
 
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
